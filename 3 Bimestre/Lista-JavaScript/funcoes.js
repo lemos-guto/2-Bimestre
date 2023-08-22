d2e6876cf2de2d7fcd90d401685c13c4b5f560a1
Gustavo Lemos <galaxywander0@gmail.com>
2023-08-22 15:29:59 -0300

    funcoes

--- 3 Bimestre/Lista-JavaScript/funcoes.js
+++ 3 Bimestre/Lista-JavaScript/funcoes.js
@@ -5,4 +5,13 @@ function imc(peso, altura){
 console.log(imc(70, 1.83))
 
 //Ex6 - Elevado ao Cubo
-function calcular
+function calcularCubo(num){
+    return Math.pow(num, 3);
+}
+console.log(calcularCubo(3))
+
+//Ex7 - Calcular Potencia
+function calcularPotencia(num, potencia){
+    return Math.pow(num, potencia)
+}
+console.log(calcularPotencia(3,3))
